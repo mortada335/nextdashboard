514d827c63344cc9a459dda21eedfe7f81bb058b
mortada335 <a80923904@gmail.com>
2025-02-22 11:11:43 +0300

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+export default function page() {
+  return (
+    <div>
+      {" "}
+      <h2>Customers page</h2>
+    </div>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function page() {
+  return (
+    <div>
+      <h1>dashboard page</h1>
+    </div>
+  );
+}
